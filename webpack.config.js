4388a95ffd0e495bd9ffc7fa79c5b55d6bb7aeb6
Slavik3011 <Slavik3011@yandex.ru>
2017-11-04 02:01:48 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    entry: [
+        'babel-polyfill',
+        'react-hot-loader/patch',
+        'webpack/hot/only-dev-server',
+        './src/main.jsx'
+    ],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        extends: path.join(__dirname, '.babelrc')
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "src"),
+        port: 5000,
+        proxy: {
+          "/api": {
+                target: "http://localhost:3000",
+                changeOrigin: true
+              }
+        }
+    }
+}
